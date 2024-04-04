6612f0fddc265ded4a7cfe95482436665a7f9001
frogulator <138589139+frogulator@users.noreply.github.com>
2024-04-04 11:37:17 +0200

    link to github code

--- 3dscript.js
+++ 3dscript.js
@@ -1,7 +1,5 @@
-import * as THREE from 'https://unpkg.com/three/build/three.module.js';
-
-// import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-import { OrbitControls } from 'https://unpkg.com/three/examples/jsm/controls/OrbitControls.js';
+import * as THREE from 'three';
+import { OrbitControls } from 'OrbitControls';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
